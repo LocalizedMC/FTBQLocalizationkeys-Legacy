7e8bbe962294262677c85eab64c730a8d2cb7551
TexTrue <3140846162@qq.com>
2023-03-13 11:11:45 +0800

    wip

--- src/main/java/org/localmc/tools/ftbqkeys/FTBQKeysModLegacy.java
+++ src/main/java/org/localmc/tools/ftbqkeys/FTBQKeysModLegacy.java
@@ -2,29 +2,35 @@ package org.localmc.tools.ftbqkeys;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import net.minecraft.init.Blocks;
+import net.minecraft.client.Minecraft;
+import net.minecraftforge.client.ClientCommandHandler;
+import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
 import org.apache.commons.io.FileUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.localmc.tools.ftbqkeys.command.FTBQKeysCommand;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
 import java.util.Locale;
 import java.util.TreeMap;
 
 @Mod(modid = FTBQKeysModLegacy.MODID, name = FTBQKeysModLegacy.NAME, version = FTBQKeysModLegacy.VERSION)
 public class FTBQKeysModLegacy {
-
     public static Logger LOGGER = LogManager.getLogger();
     public static final String MODID = "ftbqlocalkeys";
     public static final String NAME = "FTBQLocalizationKeys";
     public static final String VERSION = "1.0";
 
     public static final Gson gson = new GsonBuilder().setPrettyPrinting().create();
+    public static Path gameDir = Minecraft.getMinecraft().gameDir.toPath();
+    public static Path configDir = Loader.instance().getConfigDir().toPath();
 
     public static void saveLang(TreeMap<String, String> transKeys, String lang, File parent) throws IOException {
         File fe = new File(parent, lang.toLowerCase(Locale.ROOT) + ".lang");
@@ -38,6 +44,11 @@ public class FTBQKeysModLegacy {
 
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
+        ClientCommandHandler.instance.registerCommand(new FTBQKeysCommand());
+    }
+
+    @Mod.EventHandler
+    public void onSeverStart(FMLServerStartingEvent event) {
 
     }
 
